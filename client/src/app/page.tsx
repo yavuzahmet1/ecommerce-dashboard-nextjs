1fad52d322e7711e7760d66ab402d31fdcc234c8
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-10-12 23:02:45 +0300

    feat: implement Homepage layout with featured image and product list

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,5 +1,17 @@
+import ProductList from "@/components/ProductList";
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div className="">Homepage</div>;
+  return (
+    <div className="">
+      <div className="">
+        <div className="relative aspect-[3/1]">
+          <Image src="/featured.png" alt="featured product" fill />
+        </div>
+        <ProductList />
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
