3ecfc34561b882c7a258965e3f912d1a74174372
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-10-15 23:02:56 +0300

    feat: add cart summary details and continue button in CartPage component

--- client/src/app/cart/page.tsx
+++ client/src/app/cart/page.tsx
@@ -86,11 +86,46 @@ const CartPage = () => {
         <div className="w-full lg:w-1/2 shadow-lg border-1 border-gray-100 p-8 rounded-lg flex flex-col gap-8"></div>
         <div className="w-full lg:w-1/3 shadow-lg border-1 border-gray-100 p-8 rounded-lg flex flex-col gap-8">
           <h2 className="font-semibold">Cart Details</h2>
-          <div></div>
-          <button className="w-full bg-gray-800 text-white p-2 rounded-lg cursor-pointer flex items-center justify-center hover:bg-gray-900 transition">
-            Continue
-            <ArrowRight className="w-3 h-3 inline-block ml-2" />
-          </button>
+          <div className="flex flex-col gap-4">
+            <div className="flex justify-between">
+              <p className="text-gray-600">Subtotal</p>
+              <p className="font-semibold">
+                $
+                {cartItems
+                  .reduce((acc, item) => acc + item.price * item.quantity, 0)
+                  .toFixed(2)}
+              </p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-gray-600">Discount (10%)</p>
+              <p className="font-semibold text-red-600">-$ 10</p>
+            </div>
+            <div className="flex justify-between">
+              <p className="text-gray-600">Shipping Fee</p>
+              <p className="font-semibold">$ 10</p>
+            </div>
+            <hr className="border-gray-200" />
+            <div className="flex justify-between">
+              <p className="text-gray-800 font-semibold">Total</p>
+              <p className="font-semibold">
+                <p className="font-semibold">
+                  $
+                  {cartItems
+                    .reduce((acc, item) => acc + item.price * item.quantity, 0)
+                    .toFixed(2)}
+                </p>
+              </p>
+            </div>
+          </div>
+          {activeStep === 1 && (
+            <button
+              className="w-full bg-gray-800 text-white p-2 rounded-lg cursor-pointer flex items-center justify-center hover:bg-gray-900 transition"
+              onClick={() => router.push("/cart?step=2", { scroll: false })}
+            >
+              Continue
+              <ArrowRight className="w-3 h-3 inline-block ml-2" />
+            </button>
+          )}
         </div>
       </div>
     </div>
