4907238079248a3462a814c14332b05d891dd144
yavuzahmet1 <1yavuzahmet@gmail.com>
2025-10-18 22:42:34 +0300

    feat: add PaymentForm schema and inputs type for validation

--- client/src/types.ts
+++ client/src/types.ts
@@ -40,3 +40,31 @@ export const shippingFormSchema = z.object({
 });
 
 export type ShippingFormInputs = z.infer<typeof shippingFormSchema>;
+export const PaymentFormSchema = z.object({
+  cardHolder: z
+    .string()
+    .min(2, "Card Holder must be at least 2 characters long"),
+  cardNumber: z
+    .string()
+    .min(16, "Card Number must be 16 characters long")
+    .max(16, "Card Number must be 16 characters long")
+    .regex(/^[0-9]+$/, "Card Number can only contain numbers"),
+  expirationDate: z
+    .string()
+    .regex(
+      /^(0[1-9]|1[0-2])\/?([0-9]{2})$/,
+      "Expiration Date must be in MM/YY format"
+    ),
+  cvv: z
+    .string()
+    .min(3, "CVV must be at least 3 characters long")
+    .max(4, "CVV must be at most 4 characters long")
+    .regex(/^[0-9]+$/, "CVV can only contain numbers"),
+  address: z
+    .string()
+    .min(1, "Address is required")
+    .max(100, "Address must be at most 100 characters long"),
+  city: z.string().min(1, "City is required"),
+});
+
+export type PaymentFormInputs = z.infer<typeof PaymentFormSchema>;
